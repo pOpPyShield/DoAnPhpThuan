551796eb53aee507a063bed510e3018ff4e6c608
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2020-11-11 20:56:32 +0700

    It can not run change to begin

--- public/Asset/js/validate.js
+++ public/Asset/js/validate.js
@@ -2,7 +2,7 @@ function validate() {
     const username = document.getElementById("name").innerText;
     const password = document.getElementById("pwd").innerText;
 
-    if(username.value <= 8) {
+    if(username.value.length <= 8) {
         alert("UserName must have more than 8 characters, try again");
         return false;
     }
