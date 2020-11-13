5fc73c68ad00c79745ada3f99f7c039fac952d4c
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2020-11-13 20:15:19 +0700

    Test upload image

---
+++ upload.php
@@ -0,0 +1,33 @@
+<?php 
+
+    if(isset($_POST['submit'])) {
+            $file = $_FILES['file'];
+        $fileName = $_FILES['file']['name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileType = $_FILES['file']['type'];
+        $fileError = $_FILES['file']['error'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+        
+        $allowed = array('jpg' , 'jpeg' , 'png');
+        
+        if(in_array($fileActualExt, $allowed)) {
+            if($fileError === 0) {
+                if($fileSize<50000) {
+                    $fileNameNew = uniqid('',true). "." .$fileActualExt;
+                    $fileDestination = 'uploads/'.$fileNameNew; 
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    header('Location: ./?uploadsuccess');
+                }
+                else {
+                    echo 'There was an error';
+                }
+            }
+        } else {
+            echo 'Cannot upload';
+        }
+        
+    }
+
+?>
