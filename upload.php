f1b390e3a2a7a00523957bc77c96e0e639de9327
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2020-11-13 20:26:09 +0700

    Success test function upload IMG

--- upload.php
+++ upload.php
@@ -2,6 +2,7 @@
 
     if(isset($_POST['submit'])) {
         $file = $_FILES['file'];
+        print_r($file);
         $fileName = $_FILES['file']['name'];
         $fileSize = $_FILES['file']['size'];
         $fileType = $_FILES['file']['type'];
@@ -13,8 +14,8 @@
         $allowed = array('jpg' , 'jpeg' , 'png');
         
         if(in_array($fileActualExt, $allowed)) {
-            if($fileError === 0) {
-                if($fileSize<50000) {
+            if($fileError == 0) {
+                if($fileSize<100000) {
                     $fileNameNew = uniqid('',true). "." .$fileActualExt;
                     $fileDestination = 'uploads/'.$fileNameNew; 
                     move_uploaded_file($fileTmpName, $fileDestination);
