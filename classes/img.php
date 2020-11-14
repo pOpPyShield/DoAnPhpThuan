780e20833b2ce72b678d47860ba8a8ff0239e26f
pOpPyShield <bqhuy.19it4@vku.udn.vn>
2020-11-14 08:47:34 +0700

    Update database for testing, import it in database and use

--- classes/img.php
+++ classes/img.php
@@ -31,5 +31,31 @@
 
             return false;
         }
+
+        public function uploadImg() {
+            $file = $_FILES['file'];
+            $fileName = $_FILES['file']['name'];
+            $fileSize = $_FILES['file']['size'];
+            $fileType = $_FILES['file']['type'];
+            $fileError = $_FILES['file']['error'];
+            $fileTmpName = $_FILES['file']['tmp_name'];
+            $fileExt = explode('.', $fileName);
+            $fileActualExt = strtolower(end($fileExt));
+            $allowed = array('jpg' , 'jpeg' , 'png');
+            if(in_array($fileActualExt, $allowed)) {
+                if($fileError == 0) {
+                    if($fileSize<100000) {
+                        $fileNameNew = "profile". $this->_imgID . "." .$fileActualExt;
+                        $fileDestination = 'uploads/'.$fileNameNew; 
+                        move_uploaded_file($fileTmpName, $fileDestination);
+                        header('Location: ./?uploadsuccess');
+                        return 0;
+                    }
+                    else {
+                        return 1;
+                    }
+                }
+            }
+        }
     }
 ?>
