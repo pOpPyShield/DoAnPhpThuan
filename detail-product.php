2fb292b7fbfc3f51e3eb8875b194024d37144d67
cuminhthien031281 <67586402+cuminhthien031281@users.noreply.github.com>
2020-11-16 10:52:41 +0700

    detail-product-text

--- detail-product.php
+++ detail-product.php
@@ -40,14 +40,62 @@
                     <h4>Giá KM:
                         <p>15,390,000₫</p>
                     </h4>
-
                     <h5>Số lượng:</h5> <input type="number" value="1">
                     <a href="#" class="btn-buy-now"><span>Mua ngay</span></a>
-
                     <a href="#" class="btn-add-product"><span>Thêm vào giỏ hàng</span></a>
-
                 </div>
             </div>
+            <p>Mô tả sản phẩm</p>
+            <p>Với kinh phí dưới 10 triệu đồng nhưng lại cần build một chiếc pc chất lượng nhằm hỗ trợ công việc. Đặc biệt, đáp ứng được nhu cầu giải trí với các tựa game đình đám. </p>
+            <b><h4>GVN Titan 10 M</h4>  sẽ là một trong những sự lựa chọn tốt nhất trong phân khúc dưới 10 triệu đồng bạn không nên bỏ qua.
+Chi tiết cấu hình</b>
+            <table>
+                    <tr>
+                        <th></th>
+                        <th>Tên Sản phẩm</th>
+                        <th>Thời gian bảo hành</th>
+                    </tr>
+                    <tr>
+                        <td>Mainboard</td>
+                        <td><a href="#">Asus EX H410M-V3</a></td>
+                        <td>36 tháng</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">CPU</a></td>
+                        <td><a href="#">Intel Pentium G6400 / 4MB / 4.0GHz / 2 Nhân 4 Luồng / LGA 1200</a></td>
+                        <td>36 tháng</td>
+                    </tr>
+                    <tr>
+                        <td>RAM</td>
+                        <td><a href="#">G.SKILL Ripjaws V 1x8GB 2800</a></td>
+                        <td>36 tháng</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">VGA</a></td>
+                        <td><a href="#">ASUS TUF Gaming GeForce GTX 1650 4GB GDDR6</a></td>
+                        <td>36 tháng</td>
+                    </tr>
+                    <tr>
+                        <td>HDD</td>
+                        <td>Có thể tùy chọn <a href="#">Nâng cấp</a></td>
+                        <td>24 tháng</td>
+                    </tr>
+                    <tr>
+                        <td>SSD</td>
+                        <td><a href="#">PNY SSD CS900 120G 2.5" Sata 3</a></td>
+                        <td>36 tháng</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">PSU</a></td>
+                        <td><a href="#">Deepcool DN450 80 Plus</a></td>
+                        <td>36 tháng</td>
+                    </tr>
+                    <tr>
+                        <td><a href="#">Case</a></td>
+                        <td><a href="#">Xigmatek NYX 3F RGB ( Mini Tower )</a></td>
+                        <td>12 tháng</td>
+                    </tr>
+                    </table>
         </div>
     </div>
     <!-------END-single product details-->
